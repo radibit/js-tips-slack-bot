54f3f59bcf15c38ec3f9da0fd68f533e6f13af02
radibit <radimir.bitsov@gmail.com>
2016-02-02 10:06:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JS Tips Slack Bot
---
+++ app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var SlackBot  = require( 'slackbots' );
+var ghget     = require( 'github-get' );
+
+var bot = new SlackBot( {
+    token : process.env.SLACK_TOKEN,
+    name  : 'JS Tips'
+} );
+
+var options = {
+  token : process.env.GH_TOKEN;
+};
+
+var interval     = 7200000;
+var oldJsTip     = {};
+var jsTipMessage = '';
+
+/**
+ * Send the latest Js Tip
+ *
+ * @param  {String} path - URL path to the tip gh page
+ * @return {String}      - Console logged error message
+ */
+function sendLatestTip( path ) {
+  ghget( 'loverajoel/jstips', path, options, function( err, jsTip ) {
+    if ( err ) {
+      return console.warn( err );
+    }
+
+    jsTipMessage += '*Title:* `' + jsTip.content.substring(
+      jsTip.content.indexOf( 'tip-tldr:' ) + 9, jsTip.content.indexOf( 'categories:' )
+    ).trim() + '`';
+
+    jsTipMessage += '\n\n*URL:* ';
+
+    jsTipMessage += jsTip[ 'html_url' ];
+
+    bot.on( 'open', function() {
+        var params = {
+            icon_emoji : ':octocat:'
+        };
+
+        bot.postMessageToUser( 'radimir', jsTipMessage, params );
+    } );
+  } );
+}
+
+/**
+ * Get the latest JS Tip from the list of all available tips
+ *
+ * @return {String} - Console logged error message
+ */
+function getLatestTip() {
+  ghget( 'loverajoel/jstips', '/_posts/en/', options, function( err, filenames ) {
+    if ( err ) {
+      return console.warn( err );
+    }
+
+    var newJsTip = filenames[ filenames.length - 1 ];
+
+    if ( newJsTip.path !== oldJsTip.path ) {
+      oldJsTip = newJsTip;
+
+      sendLatestTip( newJsTip.path );
+    }
+  } );
+}
+
+getLatestTip();
+
+setInterval( getLatestTip, interval );
